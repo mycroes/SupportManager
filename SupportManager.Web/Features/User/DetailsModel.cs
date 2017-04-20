d8a28bfda86058149a2f2c0c84b62d5c718dc450
Michael Croes <mycroes@gmail.com>
2017-04-20 22:59:31 +0200

    Improve user index

--- SupportManager.Web/Features/User/DetailsModel.cs
+++ SupportManager.Web/Features/User/DetailsModel.cs
@@ -5,7 +5,9 @@ namespace SupportManager.Web.Features.User
     public class DetailsModel
     {
         public string DisplayName { get; set; }
+        public int? PrimaryEmailAddressId { get; set; }
         public string PrimaryEmailAddressValue { get; set; }
+        public int? PrimaryPhoneNumberId { get; set; }
         public string PrimaryPhoneNumberLabel { get; set; }
         public string PrimaryPhoneNumberValue { get; set; }
         public List<PhoneListModel> PhoneNumbers { get; set; }
