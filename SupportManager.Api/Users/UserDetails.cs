f1f24ada54db946eee9f882c5fee01cb52bebff2
Michael Croes <mycroes@gmail.com>
2018-12-19 21:13:48 +0100

    api: Add UserDetails.Id

--- SupportManager.Api/Users/UserDetails.cs
+++ SupportManager.Api/Users/UserDetails.cs
@@ -4,6 +4,7 @@ namespace SupportManager.Api.Users
 {
     public class UserDetails
     {
+        public int Id { get; set; }
         public string DisplayName { get; set; }
         public string Login { get; set; }
         public List<EmailAddress> EmailAddresses { get; set; }
