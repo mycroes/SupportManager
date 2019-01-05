677c5074e8d5c13291869067adff1f99e063c08e
Michael Croes <mycroes@gmail.com>
2019-01-05 22:21:09 +0100

    Refactor SupportManager.Web startup
    Allows for startup in VS again.

--- SupportManager.Web/Program.cs
+++ SupportManager.Web/Program.cs
@@ -1,25 +1,54 @@
-﻿using Topshelf;
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.HttpSys;
+using Topshelf;
 
 namespace SupportManager.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static string[] args;
+
+        public static void Main()
         {
             HostFactory.Run(cfg =>
             {
-                cfg.Service<Service>(() => new Service(args));
-
                 cfg.AddCommandLineDefinition("aspnetcoreargs", v => args = v.Split(' '));
 
                 cfg.SetServiceName("SupportManager.Web");
                 cfg.SetDisplayName("SupportManager.Web");
                 cfg.SetDescription("SupportManager Web Interface");
 
-                cfg.RunAsNetworkService();
+                cfg.Service<IWebHost>(svc =>
+                {
+                    svc.ConstructUsing(CreateWebHost);
+                    svc.WhenStarted(webHost => webHost.Start());
+                    svc.WhenStopped(webHost => webHost.Dispose());
+                });
 
+                cfg.RunAsLocalSystem();
                 cfg.StartAutomatically();
             });
+        }
+
+        private static IWebHost CreateWebHost()
+        {
+            var builder = WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+
+            if (!Debugger.IsAttached)
+            {
+                builder.UseHttpSys(options =>
+                {
+                    options.Authentication.Schemes = AuthenticationSchemes.NTLM | AuthenticationSchemes.Negotiate;
+                    options.Authentication.AllowAnonymous = true;
+                });
+
+                builder.UseContentRoot(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName));
+            }
+
+            return builder.Build();
         }
     }
 }
