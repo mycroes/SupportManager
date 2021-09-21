df24dbca6d8e00992a2943bf493d689fd7122a56
Michael Croes <mycroes@gmail.com>
2021-09-21 10:08:40 +0200

    telegram: Add sync option

--- SupportManager.Telegram/Program.cs
+++ SupportManager.Telegram/Program.cs
@@ -1,16 +1,88 @@
 ﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
+using Refit;
 using SupportManager.Telegram;
 using SupportManager.Telegram.Infrastructure;
 using Topshelf;
 
-
-if (args.Length == 2 && args[0].Equals("migrate", StringComparison.InvariantCultureIgnoreCase))
+if (args.Length > 0)
 {
-    var db = DbContextFactory.Create(args[1]);
-    db.Database.Migrate();
+    switch (args[0])
+    {
+        case "migrate":
+            if (args.Length == 2)
+            {
+                var db = DbContextFactory.Create(args[1]);
+                db.Database.Migrate();
+            }
+            else
+            {
+                Console.WriteLine("Usage: SupportManager.Telegram migrate <UserDb>");
+            }
 
-    return;
+            return;
+
+        case "sync":
+            if (args.Length == 4)
+            {
+                if (!Uri.TryCreate(args[2], UriKind.Absolute, out var url))
+                {
+                    Console.WriteLine("Couldn't parse SupportManagerUrl argument");
+                    return;
+                }
+
+                if (!Uri.TryCreate(args[3], UriKind.Absolute, out var callbackUri))
+                {
+                    Console.WriteLine("Couldn't parse CallbackUrl argument");
+                    return;
+                }
+
+                var db = DbContextFactory.Create(args[1]);
+                var callbackUrl = callbackUri.AbsoluteUri.TrimEnd('/');
+
+                using var host = WebHost.Start(callbackUrl,
+                    router => router.MapGet("/{userId}/ping", context => context.Response.WriteAsync("OK")));
+
+                foreach (var user in await db.Users.ToListAsync())
+                {
+                    Console.WriteLine($"Checking user {user.UserId}");
+
+                    try
+                    {
+                        var httpClient =
+                            new HttpClient(new AuthenticatedHttpClientHandler(user.ApiKey)) {BaseAddress = url};
+                        var api = RestService.For<ISupportManagerApi>(httpClient);
+
+                        Console.WriteLine($"Subscribing to {callbackUrl}/{user.UserId}");
+                        await api.Subscribe($"{callbackUrl}/{user.UserId}");
+
+                        var newId = (await api.MyDetails()).Id;
+                        if (newId != user.SupportManagerUserId)
+                        {
+                            Console.WriteLine(
+                                $"Updating SupportManagerUserId for {user.ApiKey} from {user.SupportManagerUserId} to {newId}");
+                            user.SupportManagerUserId = newId;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"Failed to verify user {user.UserId}");
+                    }
+                };
+
+                await db.SaveChangesAsync();
+            }
+            else
+            {
+                Console.WriteLine("Usage: SupportManager.Telegram sync <UserDb> <SupportManagerUrl> <HostUrl> <CallbackUrl>");
+            }
+
+            return;
+    }
 }
 
 HostFactory.Run(cfg =>
