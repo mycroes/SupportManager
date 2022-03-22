ceb4851d5ff8b90bf6b75b7158435988d74b6ca4
Michael Croes <mycroes@gmail.com>
2022-03-22 20:34:12 +0100

    web/ValidatorActionFilter: Cosmetical cleanup

--- SupportManager.Web/Infrastructure/ValidatorActionFilter.cs
+++ SupportManager.Web/Infrastructure/ValidatorActionFilter.cs
@@ -12,24 +12,23 @@ namespace SupportManager.Web.Infrastructure
 
         public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
         {
-            if (!context.ModelState.IsValid)
-            {
-                if (context.HttpContext.Request.Method == "GET")
-                {
-                    var result = new BadRequestResult();
-                    context.Result = result;
-                }
-                else
-                {
-                    var result = new ContentResult();
-                    string content = JsonConvert.SerializeObject(context.ModelState,
-                        new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
-                    result.Content = content;
-                    result.ContentType = "application/json";
+            if (context.ModelState.IsValid) return;
 
-                    context.HttpContext.Response.StatusCode = 400;
-                    context.Result = result;
-                }
+            if (context.HttpContext.Request.Method == "GET")
+            {
+                var result = new BadRequestResult();
+                context.Result = result;
+            }
+            else
+            {
+                var result = new ContentResult();
+                var content = JsonConvert.SerializeObject(context.ModelState,
+                    new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+                result.Content = content;
+                result.ContentType = "application/json";
+
+                context.HttpContext.Response.StatusCode = 400;
+                context.Result = result;
             }
         }
 
