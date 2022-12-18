8f2ffb4f3e68a8aa2fa0636db8617256e6461366
Michael Croes <mycroes@gmail.com>
2022-12-18 21:21:59 +0100

    fix: Remove Required attributes from TeamMember relations
    Fix team member add which wasn't working previously because the EF
    validation would block insert with foreign keys only set on for any of
    the relational properties.

--- SupportManager.DAL/TeamMember.cs
+++ SupportManager.DAL/TeamMember.cs
@@ -5,11 +5,9 @@ namespace SupportManager.DAL
 {
     public class TeamMember
     {
-        [Required]
         public virtual SupportTeam Team { get; set; }
         [Key, ForeignKey(nameof(Team)), Column(Order = 1)]
         public virtual int TeamId { get; set; }
-        [Required]
         public virtual User User { get; set; }
         [Key, ForeignKey(nameof(User)), Column(Order = 2)]
         public virtual int UserId { get; set; }
