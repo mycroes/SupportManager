d9a1f2c5820b21b7906652e367c158795d4620e8
Michael Croes <mycroes@gmail.com>
2024-08-28 13:32:21 +0200

    feat: Add CRUD records

---
+++ SupportManager.Web/Infrastructure/CRUD/Create.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace SupportManager.Web.Infrastructure.CRUD;
+
+public record Create<T>(T Model) : IRequest;
---
+++ SupportManager.Web/Infrastructure/CRUD/Index.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace SupportManager.Web.Infrastructure.CRUD;
+
+public record Index<T> : IRequest<List<Record<T>>>;
